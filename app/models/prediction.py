005d955875e9f6b79e8d046957689388754dc706
Joaconjoa <92233565+Joaconjoa@users.noreply.github.com>
2022-07-09 14:37:43 +0200

    Update prediction.py

--- app/models/prediction.py
+++ app/models/prediction.py
@@ -1,9 +1,9 @@
 from pydantic import BaseModel
 
-from app.core.enums import Sentiment
+from app.core.enums import Troll
 
 
-class SentimentPredictionResult(BaseModel):
-    label: Sentiment
+class TrollPredictionResult(BaseModel):
+    label: Troll
     score: float
     elapsed_time: float
